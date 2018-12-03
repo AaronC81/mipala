3d200829fa4a48e9f33abc5419641b4b092e4226
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-03 16:31:47 +0000

    Add Token class to default requires

--- src/mipala.rb
+++ src/mipala.rb
@@ -6,6 +6,7 @@ require_relative 'mipala/extensions/hash'
 require_relative 'mipala/parser/constants'
 require_relative 'mipala/parser/location'
 require_relative 'mipala/parser/symbol_locator'
+require_relative 'mipala/parser/token'
 require_relative 'mipala/parser/tokenizer'
 
 require_relative 'mipala/elements/element'
