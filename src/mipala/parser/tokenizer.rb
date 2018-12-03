aae730c3a5cd19a2c19849306534fa8fb8d30df5
Aaron Christiansen <aaronc20000@gmail.com>
2018-12-03 14:27:43 +0000

    Add buggy space-to-indent converter (doesn't work if there are spaces at the beginning of non-newline text nodes)

--- src/mipala/parser/tokenizer.rb
+++ src/mipala/parser/tokenizer.rb
@@ -67,6 +67,7 @@ module Mipala::Parser
 
         # Create tokens from these segments
         line_segments.flat_map do |seg|
+          p seg
           # Count the number of spaces at the beginning of the string
           spaces_at_start = seg.scan(/^ */).first.length
 
@@ -78,7 +79,42 @@ module Mipala::Parser
         end
       end
 
-      # TODO: Convert spaces into indent/dedent, maybe separate step
+      # Convert :space_count tokens to indentation
+      current_indentation_level = 0
+      indentation_size = nil
+
+      p tokens_with_space_counts
+
+      tokens_with_indents = tokens_with_space_counts.map do |token|
+        # Only look for :space_count tokens
+        next token if token.type != :space_count
+
+        # Set indentation size if this is the first indentation we encounter
+        indentation_size = token.value if indentation_size.nil? && \
+          !token.value.zero?
+
+        # If this isn't an indentation but we don't know the size yet, skip
+        next token if indentation_size.nil?
+
+        # +1.0 if indent, -1.0 if dedent, 0 if no change, anything else is error
+        indentation_delta = (token.value - current_indentation_level.to_f) \
+          / indentation_size
+
+        p indentation_size
+
+        case indentation_delta
+        when 1.0
+          Token.new(:indent, nil)
+        when -1.0
+          Token.new(:dedent, nil)
+        when 0
+          nil
+        else
+          raise "invalid indentation (delta #{indentation_delta})"
+        end
+      end.reject(&:nil?)
+
+      tokens_with_indents
     end
   end
 end
